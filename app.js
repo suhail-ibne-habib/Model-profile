f03c40ad04d1c8ad8797cd8491a265a66302dd9b
Suhail Habib <suhailibnehabib@gmail.com>
2022-06-09 09:36:24 +0600

    All Files

---
+++ app.js
@@ -0,0 +1,52 @@
+$('.bgs').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    speed: 500
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+  const tl = gsap.timeline();
+
+  tl.from( "#bg", {
+      scale: 1.2,
+      duration: 0.5,
+      scrollTrigger: {
+          trigger: '#one',
+          start: 'left left'
+        }
+  } )
+
+  tl.from( '#tiktok', {
+      x : '-100vw',
+      duration: 0.3,
+      ease: "power4.out"
+  } )
+
+  tl.from( '#insta', {
+      x : '100vw',
+      duration: 0.3,
+      ease: "power4.out"
+  } )
+
+  tl.from( '#onlyfans', {
+      x : '-100vw',
+      duration: 0.3,
+      ease: "power4.out"
+  } )
+
+  tl.from( '#onlyfans2', {
+      x : '100vw',
+      duration: 0.3,
+      ease: "power4.out"
+  } )
+
+  tl.from( '#shop', {
+      x : '-100vw',
+      duration: 0.3,
+      ease: "power4.out"
+  } )
+          
