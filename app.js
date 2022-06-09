d011fe5ece31b45c29432a5f1c9243f7743c1d18
Suhail Habib <suhailibnehabib@gmail.com>
2022-06-09 10:45:24 +0600

    Bug fixed, added more links

--- app.js
+++ app.js
@@ -12,17 +12,12 @@ $('.bgs').slick({
   const tl = gsap.timeline();
 
   tl.from( "#bg", {
-      scale: 1.1,
+      scale: 1.2,
       duration: 0.5,
       scrollTrigger: {
           trigger: '#bg',
+          start: 'left left'
         }
-  } )
-
-  tl.from( '#tiktok', {
-      x : '-100vw',
-      duration: 0.3,
-      ease: "power4.out"
   } )
 
   tl.from( '#insta', {
@@ -30,6 +25,18 @@ $('.bgs').slick({
       duration: 0.3,
       ease: "power4.out"
   } )
+  
+  tl.from( '#tiktok', {
+      x : '-100vw',
+      duration: 0.3,
+      ease: "power4.out"
+  } )
+
+  tl.from( '#twitter', {
+    x : '-100vw',
+    duration: 0.3,
+    ease: "power4.out"
+} )
 
   tl.from( '#onlyfans', {
       x : '-100vw',
@@ -44,6 +51,12 @@ $('.bgs').slick({
   } )
 
   tl.from( '#shop', {
+      x : '-100vw',
+      duration: 0.3,
+      ease: "power4.out"
+  } )
+  
+  tl.from( '#cash', {
       x : '-100vw',
       duration: 0.3,
       ease: "power4.out"
